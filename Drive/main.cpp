f40bc5f1395e1c8cf42353058db46a31cda6d269
Tony Le <letony@icloud.com>
2018-05-23 14:25:34 +1000

    Drive initial commit

---
+++ Drive/main.cpp
@@ -0,0 +1,15 @@
+#include "DriveApp.h"
+
+int main() {
+	
+	// allocation
+	auto app = new DriveApp();
+
+	// initialise and loop
+	app->run("AIE", 1280, 720, false);
+
+	// deallocation
+	delete app;
+
+	return 0;
+}
