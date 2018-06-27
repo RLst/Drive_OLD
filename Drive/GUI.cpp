2e65ac8316f1d68e120cdf4b0f404a240db60185
Tony Le <letony@icloud.com>
2018-06-27 17:33:13 +1000

    More gui stuff

--- Drive/GUI.cpp
+++ Drive/GUI.cpp
@@ -5,15 +5,16 @@
 void GUI::draw(Car * car)
 {
 	ImGui::Begin("Car");
-	ImGui::Text("RPM: %d", (int)car->calcNewRPM());
-	ImGui::Text("Velocity: %d", (int)car->getVelocity());
-	//Steering angle (delta)
+	ImGui::Text("Gear: %s", car->getGEARstr());
+	ImGui::Text("RPM: %d", (int)car->getRPM());
+	ImGui::Text("Velocity: %d", (int)car->getVelocity());				//World velocity vector
+	ImGui::Text("Steering angle: %d", (int)car->getSteeringAngle());	//Steering angle (delta)
 	//Side slip angle (beta)
 	//Rotation angle (yaw rate)
 	//Slip angle front wheel
 	//Slip angle rear wheels
 	//Local velocity vector
-	//World velocity vector
+	
 	ImGui::End();
 
 	//std::cout << "Gear: " << m_current_gear << std::endl;
