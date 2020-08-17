4c9a2a2bdb8a86d8f09458155f05c8e163b170c2
Tony Le <letony@icloud.com>
2020-08-17 22:05:01 +1000

    Add github plugin

--- Assets/Scripts/ExtraControls.cs
+++ Assets/Scripts/ExtraControls.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VehiclePhysics;
 
 public class ExtraControls : MonoBehaviour
 {
+    VPVehicleController controller;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // SteeringAids.Apply()
+        // controller.steeringAids.
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
